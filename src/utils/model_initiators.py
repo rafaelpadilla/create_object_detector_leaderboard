d06e94dfba9e9f02a1168bbdc24fb795144b3073
Rafael <eng.rafaelpadilla@gmail.com>
2023-08-23 15:44:28 +0000

    refactoring model_initiators

--- src/utils/model_initiators.py
+++ src/utils/model_initiators.py
@@ -2,9 +2,31 @@ from transformers import  AutoModelForObjectDetection, DetrForObjectDetection, O
 from ultralyticsplus import YOLO
 import yolov5
 import yolov7
+from ..hf_models.detr import Detr
+from ..hf_models.owl import Owl
+from ..hf_models.deformable_detr import DeformableDetr
+from ..hf_models.deta import Deta
+from ..hf_models.yolo import Yolo
+from ..hf_models.yolo_ultralytics import YoloUltralytics
+from ..hf_models.yolov5 import Yolov5
+from ..hf_models.conditional_detr import ConditionalDetr
+
+# Maps model's class name to a class in hf_models
+dict_architecture = {"transformers.models.detr.modeling_detr.DetrForObjectDetection": Detr,
+                     "transformers.models.owlvit.modeling_owlvit.OwlViTForObjectDetection": Owl,
+                     "yolov5.models.common.AutoShape": Yolov5,
+                     "ultralyticsplus.ultralytics_utils.YOLO": YoloUltralytics, 
+                     "transformers.models.yolos.modeling_yolos.YolosForObjectDetection": Yolo,
+                     "transformers.models.conditional_detr.modeling_conditional_detr.ConditionalDetrForObjectDetection": ConditionalDetr,
+                     "transformers.models.deta.modeling_deta.DetaForObjectDetection": Deta,
+                     "transformers.models.deformable_detr.modeling_deformable_detr.DeformableDetrForObjectDetection": DeformableDetr
+                     }
+
+dict_model_initiator = {"owlvit-base-patch32": OwlViTForObjectDetection,
+                        "owlvit-large-patch14": OwlViTForObjectDetection,
+                        "owlvit-base-patch16": OwlViTForObjectDetection,}
 
 models_initiators = [AutoModelForObjectDetection, DeformableDetrForObjectDetection, DetrForObjectDetection, OwlViTForObjectDetection, YOLO, yolov5, yolov7, YolosForObjectDetection, ConditionalDetrForObjectDetection, DetaForObjectDetection]
-# image_processors = [AutoImageProcessor, DetrImageProcessor]
 
 def get_classes_from_model(model):
     if hasattr(model, "config") and hasattr(model.config, "label2id"):
@@ -29,11 +51,6 @@ def try_create_model(model_class, model_id):
         pass
     return None
 
-def try_create_processor(img_processor, model_id):
-    try:
-        return img_processor.from_pretrained(model_id)
-    except:
-        pass
 
 def get_full_class_name(obj):
     klass = obj.__class__
@@ -43,23 +60,40 @@ def get_full_class_name(obj):
         return klass.__qualname__ 
     return module + '.' + klass.__qualname__
 
+def _split_org_model(model_id: str) -> str:
+    sep = "/"
+    if sep in model_id:
+        return model_id.split(sep)
+    return model_id, ""
     
 def initiate_model(model_id):
-    for model_initiator in models_initiators:
+    # First check if model is defined in dict_model_initiator
+    _, model_name = _split_org_model(model_id)
+    _class = dict_model_initiator.get(model_name)
+    
+    lst_model_initiators = models_initiators
+    
+    # If model is not mapped in dict_model_initiator
+    if _class is not None:
+        lst_model_initiators = [_class]
+    
+    for model_initiator in lst_model_initiators:
         model = try_create_model(model_initiator, model_id)
         if model is not None:
             return model
     return None
 
-# def initiate_processor(model_id):
-#     for img_processor in image_processors:
-#         processor = try_create_processor(img_processor, model_id)   
-#         if processor is not None:
-#             return processor
-#     return None
 
-# def infer_architecture(model):
-#     if hasattr(model, "config") and hasattr(model.config, "architectures"):
-#         return model.config.architectures
-#     else:
-#         return []
+def load_model_predictor_from_name(model_id: str, device: str = "cuda", return_batch: bool = False, threshold: float = 0.):
+    # Try to load a model
+    model = initiate_model(model_id)
+    
+    if model is None:    
+        # raise ValueError(f"Unable to load model {model_id}")
+        print(f"Unable to load model {model_id}")
+        return None
+    
+    class_name = get_full_class_name(model)
+    _class = dict_architecture[class_name]
+    return _class(model, model_id, device, return_batch, threshold)
+
