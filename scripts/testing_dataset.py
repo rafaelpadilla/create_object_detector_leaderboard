24624b2c2501adfe2c54347216e14a31142d921d
Rafael <eng.rafaelpadilla@gmail.com>
2023-09-01 19:22:30 +0000

    changing scripts folder + including FPS + elapsed time

--- scripts/testing_dataset.py
+++ scripts/testing_dataset.py
@@ -1,5 +1,5 @@
 # Example: 
-# python testing_dataset.py --path_models_csv ~/my_files/object_detection_models-08-08-2023.csv --output_folder ~/my_files/results
+# python scripts/testing_dataset.py --path_models_csv ~/my_files/object_detection_models-08-08-2023.csv --output_folder ~/my_files/results
 
 from fire import Fire
 import evaluate
@@ -12,17 +12,21 @@ from pathlib import Path
 import pandas as pd
 import ast 
 import json
+import time
+
 from cocodataset import create_json_COCO_format
 from cocodataset import COCODataset
 
 from src.utils.basics import draw_and_save_boxes, get_current_date_time, map_func
-from src.hf_models import load_model_predictor_from_name
-MODELS_TO_SKIP = ["nielsr/deta-swin-large", "nielsr/deta-resnet-50", 
-                  "nielsr/detr-resnet-50", "deepnight-research/object-detection-small", 
-                  "hf-internal-testing/tiny-random-yolos", "nielsr/detr-resnet-50-new", 
+from src.utils.model_initiators import load_model_predictor_from_name
+
+MODELS_TO_SKIP = ["deepnight-research/object-detection-small", 
+                  "hf-internal-testing/tiny-random-yolos",  
                   "api19750904/cupra", "connorhoehn/detr_trading_card_display_detection_v1", 
                   "Amogh06/detr-for-table-detection", "Amogh06/detr-for-table-detection-v3", 
-                  "nielsr/detr-finetuned-boat-detection", "SenseTime/deformable-detr-with-box-refine-two-stage"]
+                  "SenseTime/deformable-detr-with-box-refine-two-stage"]
+
+AUTHORS_TO_SKIP = ["nielsr", "amyeroberts"]
 
 COCO_IGNORED_CLASSES = [
         "None",
@@ -39,7 +43,7 @@ COCO_IGNORED_CLASSES = [
         "hair brush",    
     ]
 
-def benchmark_model(model_id, device, loaded_json_gts, coco_gt, batch_size):
+def benchmark_model(model_id, device, loaded_json_gts, coco_gt, batch_size, warm_up=False):
     
     model_predictor = load_model_predictor_from_name(model_id, device=device, return_batch=True)
     
@@ -51,16 +55,27 @@ def benchmark_model(model_id, device, loaded_json_gts, coco_gt, batch_size):
     
     print(f"\nEvaluation of model {model_id} started:")
     
-    # Clean classes
     evaluator = evaluate.load("rafaelpadilla/detection_metrics", json_gt=loaded_json_gts, iou_type="bbox")
-    # Clean classes
 
     # Prepare dataloader
     num_workers = multiprocessing.cpu_count()
     val_dataloader = DataLoader(coco_gt, batch_size=batch_size, 
                                 num_workers=num_workers, 
                                 collate_fn=model_predictor.collate_fn)
+    
+    # Warm up
+    if warm_up:
+        warm_up_batches = warm_up
+        print(f"Warming up for {warm_up_batches} batches")
+        with torch.no_grad():
+            pbar = tqdm(val_dataloader, desc="Evaluating batches")
+            for idx, batch in enumerate(pbar, 1):
+                predictions = model_predictor.predict_boxes(batch)
+                if idx == warm_up_batches:
+                    break
 
+    start_time = time.time()
+    
     # Loop over the dataset
     with torch.no_grad():
         pbar = tqdm(val_dataloader, desc="Evaluating batches")
@@ -83,10 +98,14 @@ def benchmark_model(model_id, device, loaded_json_gts, coco_gt, batch_size):
             evaluator.add(prediction=predictions, reference=image_ids)
             
             del batch
-
+            
+    # Calculate elapsed time
+    elapsed_time = time.time() - start_time
+    
     print(f"Results model: {model_id}")
     results = evaluator.compute()
-    return results
+    print(f"Elapsed time: {elapsed_time} s")
+    return results, elapsed_time
 
 def are_classes_comparable(lst_target_classes, dict_output_classes):
     keys = list(dict_output_classes.keys())
@@ -115,6 +134,9 @@ def are_classes_comparable(lst_target_classes, dict_output_classes):
 
 def main(path_models_csv: Path, output_folder: Path, dataset_name: str = "rafaelpadilla/coco2017", batch_size = 1):
     
+    path_models_csv = Path(path_models_csv)
+    output_folder = Path(output_folder)
+    
     device = "cuda:0"
 
     # Get benchmarking dataset
@@ -130,8 +152,14 @@ def main(path_models_csv: Path, output_folder: Path, dataset_name: str = "rafael
         
         output_classes_dict = ast.literal_eval(row["output_classes"])
         
-        if are_classes_comparable(target_classes, output_classes_dict):
-            models_to_benchmark.append(row["id"])
+        if not are_classes_comparable(target_classes, output_classes_dict):
+            print(f"Skipping model {row['id']} (Classes are not comparable)")
+        elif row["author"] in AUTHORS_TO_SKIP:
+            print(f"Skipping model {row['id']} (Author)")
+        elif row["id"] in MODELS_TO_SKIP:
+            print(f"Skipping model {row['id']} (Id)")
+        else:
+            models_to_benchmark.append({"model_id": row["id"], "hub_license": row["hub_license"]})
     
     # Create a JSON in memory that represents the COCO dataset
     ids_mapping, loaded_json_gts = create_json_COCO_format(benchmark_dataset, round_approx=2)
@@ -141,12 +169,11 @@ def main(path_models_csv: Path, output_folder: Path, dataset_name: str = "rafael
 
     output_folder.mkdir(parents=True, exist_ok=True)
     
-    for model_id in models_to_benchmark:
+    for model in models_to_benchmark:
         
-        # Skipping models that require further investigation
-        if model_id in MODELS_TO_SKIP:
-            continue
-        
+        model_id = model["model_id"]
+        hub_license = model["hub_license"]
+               
         # Get info to save
         date_time = get_current_date_time()
         str_model_id = model_id.replace("/","@")
@@ -158,12 +185,17 @@ def main(path_models_csv: Path, output_folder: Path, dataset_name: str = "rafael
             continue
         
         try:
-            # Run benchmark
-            metrics = benchmark_model(model_id = model_id,
+            metrics, elapsed_time = benchmark_model(model_id = model_id,
                                     device=device, 
                                     loaded_json_gts=loaded_json_gts, 
                                     coco_gt=coco_gt,
-                                    batch_size=batch_size)
+                                    batch_size=batch_size, 
+                                    warm_up = 20)
+            
+            metrics["iou_bbox"]["elapsed_time"] = elapsed_time
+            metrics["iou_bbox"]["mean_time_per_sample"] = elapsed_time/len(coco_gt)
+            metrics["iou_bbox"]["estimated_fps"] = len(coco_gt)/elapsed_time
+            metrics["iou_bbox"]["hub_license"] = hub_license
             # Save
             with open(fp, "w") as file:
                 json.dump(metrics, file, indent=4)
@@ -171,4 +203,10 @@ def main(path_models_csv: Path, output_folder: Path, dataset_name: str = "rafael
             print(f"{e}: Error model {model_id}")
 
 if __name__ == '__main__':
-    Fire(main)  
+    # Fire(main)  
+    path_models_csv = "object_detection_models-Sep-01-2023_17h45m48s.csv"
+    output_folder = "/data/results"
+    dataset_name = "rafaelpadilla/coco2017"
+    batch_size = 1
+    main(path_models_csv, output_folder, dataset_name, batch_size)
+    
