316e3af410c9bfb86d962240ae962ceff63d1c5e
Rafael <eng.rafaelpadilla@gmail.com>
2023-09-01 20:55:50 +0000

    fix imports during installation

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,7 @@
-from dotenv import load_dotenv
-
-load_dotenv()
-__version__ = "0.0.2"
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except:
+    print("Error importing dotenv")
+    
+__version__ = "0.0.3"
