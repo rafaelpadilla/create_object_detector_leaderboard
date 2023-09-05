507fc16b45367971fb0ddd4f50880e53422c9d81
Rafael Padilla <rafael.padilla@huggingface.co>
2023-09-05 23:15:40 +0000

    allowing pip install

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,8 @@
-from dotenv import load_dotenv
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except:
+    print("Error importing dotenv")
+    
+__version__ = "0.0.3"
 
-load_dotenv()
-__version__ = "0.0.2"
